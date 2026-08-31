dc94bc1ab498326d0b592935e8930636a5ef1938
N-Haitu31 <nguyenhaitu31@gmail.com>
2026-08-31 22:32:21 +0200

    style: format SQL DDL script for better readability

--- Modelisation_Relationnelle/scripts/init_database.sql
+++ Modelisation_Relationnelle/scripts/init_database.sql
@@ -7,23 +7,23 @@ BEGIN;
 DROP TABLE IF EXISTS ligne_commande, commande, produit, client CASCADE;
 
 CREATE TABLE CLIENT (
-id_client UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-nom VARCHAR NOT NULL,
-contact VARCHAR NOT NULL
+    id_client UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    nom VARCHAR NOT NULL,
+    contact VARCHAR NOT NULL
 );
 
 CREATE TABLE COMMANDE (
-id_cmd VARCHAR PRIMARY KEY,
-id_client UUID NOT NULL REFERENCES client(id_client) ON DELETE RESTRICT,
-date_achat DATE NOT NULL,
-statut_cmd VARCHAR NOT NULL,
-adr_livraison VARCHAR NOT NULL
+    id_cmd VARCHAR PRIMARY KEY,
+    id_client UUID NOT NULL REFERENCES client(id_client) ON DELETE RESTRICT,
+    date_achat DATE NOT NULL,
+    statut_cmd VARCHAR NOT NULL,
+    adr_livraison VARCHAR NOT NULL
 );
 
 CREATE TABLE PRODUIT (
-code_prod VARCHAR PRIMARY KEY,
-designation VARCHAR NOT NULL,
-prix_unitaire_ht DECIMAL(10,2) CHECK (prix_unitaire_ht >= 0) NOT NULL
+    code_prod VARCHAR PRIMARY KEY,
+    designation VARCHAR NOT NULL,
+    prix_unitaire_ht DECIMAL(10,2) CHECK (prix_unitaire_ht >= 0) NOT NULL
 );
 
 CREATE TABLE ligne_commande (
@@ -33,6 +33,5 @@ CREATE TABLE ligne_commande (
     prix_unitaire_facture DECIMAL(10,2),
     PRIMARY KEY (id_cmd, code_prod)
 );
-
 
 COMMIT;
